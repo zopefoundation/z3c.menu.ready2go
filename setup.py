354b8d8fa2eda490634939961ce13bf77df57d51
Roger Ineichen <roger@projekt01.ch>
2008-04-11 13:00:51 +0000

    Mark as dev

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
 
 setup (
     name='z3c.menu.ready2go',
-    version='0.5.0',
+    version='0.5.1dev',
     author = "Stephan Richter, Roger Ineichen and the Zope Community",
     author_email = "zope3-dev@zope.org",
     description = "A ready to go menu for Zope3",
