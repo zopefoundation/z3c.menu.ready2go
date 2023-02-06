5a34bd123e204cf9da2260aadc70199d0dd9e4fc
Michael Howitz <mh@gocept.com>
2023-02-06 15:09:37 +0100

    Bumped version for breaking release.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ TESTS_REQUIRE = [
 
 setup(
     name='z3c.menu.ready2go',
-    version='1.1.1.dev0',
+    version='2.0.dev0',
     author="Stephan Richter, Roger Ineichen and the Zope Community",
     author_email="zope-dev@zope.org",
     description="A ready to go menu for Zope3",
